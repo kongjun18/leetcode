09a2f9da5b19aef6de69bc3f8f2b9177f43211d1
Kong Jun <kongjun18@outlook.com>
2022-03-19 10:53:19 +0800

    reverse-linked-list-206

---
+++ reverse-linked-list-206.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+  ListNode *reverseList(ListNode *head) {
+    ListNode *prev = nullptr;
+    ListNode *curr = head;
+    while (curr) {
+      ListNode *next = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = next;
+    }
+    return prev;
+  }
+};
