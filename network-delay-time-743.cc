c59a7fb0e1970b13f08d04be7af4ce64820cb8d8
Kong Jun <kongjun18@outlook.com>
2022-08-11 22:34:55 +0800

    add BFS-based solution for network-delay-time-743.cc

--- network-delay-time-743.cc
+++ network-delay-time-743.cc
@@ -1,4 +1,39 @@
-// Dijkstra 算法
+// 基于 BFS 和优先级队列的 Dijkstra 算法
+class Solution {
+public:
+  int networkDelayTime(vector<vector<int>> &times, int n, int k) {
+    const int infinity = INT_MAX / 2;
+    vector<vector<pair<int, int>>> graph(n);
+    vector<int> dist(n, infinity);
+    for (const auto &edge : times) {
+      graph[edge[0] - 1].push_back(make_pair(edge[1]-1, edge[2]));
+    }
+    auto cmp = [&](int a, int b) {
+      return dist[a] < dist[b];
+    };
+    priority_queue<int, vector<int>, decltype(cmp)> q(cmp);
+    dist[k-1] = 0;
+    q.push(k-1);
+    while (!q.empty()) {
+      int v = q.top();
+      q.pop();
+      for (const auto &edge : graph[v]) {
+        if (dist[v] + edge.second < dist[edge.first]) {
+          dist[edge.first] = dist[v]+edge.second;
+          q.push(edge.first);
+        }
+      }
+    }
+    auto max_time = *max_element(dist.cbegin(), dist.cend());
+    if (max_time == infinity) {
+      return -1;
+    }
+    return max_time;
+  }
+};
+
+// 基于矩阵与动态规划的 Dijkstra 算法
+// 这个算法不好理解和实现
 class Solution {
 public:
   int networkDelayTime(vector<vector<int>> &times, int n, int k) {
@@ -32,3 +67,4 @@ public:
     return max_time == infinity ? -1 : max_time;
   }
 };
+
