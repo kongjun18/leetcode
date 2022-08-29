4e61f094855f6bf787ad66a337d2b3df3c331c0b
Kong Jun <kongjun18@outlook.com>
2022-08-29 09:55:43 +0800

    fix best-time-to-buy-and-sell-stock-ii-122.cc

--- best-time-to-buy-and-sell-stock-ii-122.cc
+++ best-time-to-buy-and-sell-stock-ii-122.cc
@@ -17,9 +17,8 @@ public:
 
 // 动态规划
 //
-// 每日股票只有两种状态，买入或卖出，所以股民手中的钱要么是买入股票后剩
-// 下的钱（dp[i][0]），要么是卖出股票赚取利润后的钱（dp[i][1]）。最大利
-// 润为最后一天的两种可能的钱的最大值。
+// 每日股票只有两种状态，买入或卖出，dp[i][0] 表示第 i 天为买入状态（第 i 天买
+// 入或之前买入但未卖）的利润，dp[i][1] 表示第 i 天为卖出状态的利润。
 //
 // 若 i-1 日卖出股票
 // - dp[i][0] = dp[i-1][1] - prices[i]
@@ -27,20 +26,24 @@ public:
 //
 // 若 i-1 日买入股票
 // - dp[i][0] = dp[i][0]
-// - dp[i][1] = dp[i-1][1] - prices[i]
+// - dp[i][1] = dp[i-1][1] + prices[i]
 //
-// 综上，dp[i][0] = max(dp[i-1][1] - prices[i], dp[i][0])
-//       dp[i][1] = max(dp[i-1][1], dp[i-1][1] - prices[i])
+// 得到递推公式
+// dp[i][0] = max(dp[i -1 ][0], dp[i - 1][1] - prices[i]);
+// dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+//
+// 考虑初始条件
+// 第 0 天买入后的利润为 -prices[0]，卖出后的利润为 0。
 class Solution {
 public:
   int maxProfit(vector<int> &prices) {
     int dp[prices.size()][2];
-    dp[0][0] = 0;
-    dp[0][1] = prices[0];
+    dp[0][0] = -prices[0];
+    dp[0][1] = 0;
     for (int i = 1; i < prices.size(); ++i) {
-      dp[i][0] = max(dp[i - 1][1] - prices[i], dp[i][0]);
-      dp[i][1] = max(dp[i - 1][1], dp[i - 1][1] - prices[i]);
+      dp[i][0] = max(dp[i -1 ][0], dp[i - 1][1] - prices[i]);
+      dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
     }
-    return max(dp[prices.size() - 1][0], dp[prices.size() - 1][1]);
+    return dp[prices.size() - 1][1];
   }
 };
