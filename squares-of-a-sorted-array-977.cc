043caa7554ba4d440e41a9bfb50d8cb437f3355d
Kong Jun <kongjun18@outlook.com>
2023-10-08 09:01:02 +0800

    update squares-of-a-sorted-array-977

--- squares-of-a-sorted-array-977.cc
+++ squares-of-a-sorted-array-977.cc
@@ -3,23 +3,24 @@
 // 元素值离 0 越远，平方越大，因此直接平方后，元素平方从数组两边向中间单
 // 调递减。
 //
-// 计算平方时，从两端向中间遍历，取平方最大（绝对值最大）的元素平方，结
-// 果数组倒序存放结果（从后往前写结果，即从后往前看单调递减，反过来看就
-// 是单调递增）。
+// 计算平方时，从两端向中间遍历，取平方最大的元素，结果数组倒序存放结果
+// （从后往前写结果，即从后往前看单调递减，反过来看就是单调递增）。
 class Solution {
 public:
   vector<int> sortedSquares(vector<int> &nums) {
-    int left = 0;
-    int right = nums.size() - 1;
-    int i = right;
-    vector<int> res(nums.size());
-    while (left <= right) {
-      if (abs(nums[left]) > abs(nums[right])) {
-        res[i--] = pow(nums[left++], 2);
+    vector<int> square(nums.size());
+    int k = nums.size() - 1;
+    for (int i = 0, j = nums.size() - 1; i <= j;) {
+      int left = nums[i] * nums[i];
+      int right = nums[j] * nums[j];
+      if (left <= right) {
+        square[k--] = right;
+        j--;
       } else {
-        res[i--] = pow(nums[right--], 2);
+        square[k--] = left;
+        i++;
       }
     }
-    return res;
+    return square;
   }
 };
