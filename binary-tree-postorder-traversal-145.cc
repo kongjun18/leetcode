b3ae3940d27b6c0b14328bcd28705bb29864356e
Jun Kong <kongjun18@outlook.com>
2024-02-21 22:28:21 +0800

    update binary-tree-postorder-traversal-145

--- binary-tree-postorder-traversal-145.cc
+++ binary-tree-postorder-traversal-145.cc
@@ -15,47 +15,46 @@
 //   是之前的右孩子，导致重复压栈。按层处理，每层考虑将左右子树压栈。
 // - 避免重复遍历。栈顶元素是父亲，导致出栈后继续遍历导致重复遍历死循环。回溯时
 //   判断栈顶元素是父亲还是右兄弟，只在栈顶是右兄弟时跳转到右兄弟继续遍历。
-
-// class Solution {
-// public:
-//   vector<int> postorderTraversal(TreeNode *root) {
-//     TreeNode *curr = root;
-//     vector<int> result;
-//     stack<TreeNode *> s;
-//     if (root)
-//       s.push(root);
-//     while (curr || !s.empty()) {
-//       if (curr) {
-//         if (curr->left) {
-//           if (curr->right) {
-//             s.push(curr->right);
-//           }
-//           s.push(curr->left);
-//           curr = curr->left;
-//         } else {
-//           if (curr->right) {
-//             s.push(curr->right);
-//           }
-//           curr = curr->right;
-//         }
-//       } else {
-//         curr = s.top(); // 栈顶是可处理的节点
-//         s.pop();
-//         result.push_back(curr->val);
-//         // 如果堆栈非空，回溯。栈顶可能是右兄弟也可能是父亲。
-//         // 如果是父亲，意味着父亲节点的孩子都已经处理完了，处理父亲节点，置空 curr（避免再次遍历）。
-//         // 如果是右兄弟，跳到右兄弟中继续遍历。
-//         if (!s.empty() && curr != s.top()->left && curr != s.top()->right) {
-//           curr = s.top();
-//         } else {
-//           curr = nullptr;
-//         }
-//       }
-//     }
-//     return result;
-//   }
-// };
-
+// - 何时回溯。当栈顶是父亲时回溯。
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;
+        TreeNode *curr = root;
+        vector<int> res;
+        if (root) {
+            s.push(root);
+        }
+        while (curr || !s.empty()) {
+            if (curr) {
+                TreeNode *next = nullptr;
+                if (curr->right) {
+                    s.push(curr->right);
+                    next = curr->right;
+                }
+                if (curr->left) {
+                    s.push(curr->left);
+                    next = curr->left;
+                }
+                curr = next;
+            } else {
+                curr = s.top();
+                s.pop();
+                res.push_back(curr->val);
+                // 往右走
+                // 栈顶要么是父亲，要么是兄弟。通过栈顶判断是否存在右兄弟。
+                if (!s.empty() && (s.top()->left != curr) && (s.top()->right != curr)) {
+                    // curr 不会进栈，因此不能 pop
+                   curr = s.top();
+                // 回溯
+                } else {
+                    curr = nullptr;
+                }
+            }
+        }
+        return res;
+    }
+};
 
 class Solution {
 public:
@@ -67,6 +66,7 @@ public:
       s.push(root);
     while (!s.empty()) {
       if (curr != s.top()->left && curr != s.top()->right) {
+        // 这里控制往左还是往右推进
         while (curr = s.top()) {
           if (curr->left) {
             if (curr->right) {
