550acf5acf8b0825a5e46cc95fef20a19956bbe4
Jun Kong <kongjun18@outlook.com>
2024-02-26 21:21:04 +0800

    update validate-binary-search-tree-98.cc

--- validate-binary-search-tree-98.cc
+++ validate-binary-search-tree-98.cc
@@ -1,3 +1,8 @@
+// 二叉搜索树的左子树的值小于当前节点，右子树的值大于当前节点。
+// 即对于每个子树，左子树树根必定小于父亲，右子树必定大于父亲。
+// 从上往下递归判断每个子树树根是否符合条件即可。
+//
+// 错误思路：查找左子树最大值和右子树最小值和根节点比较。
 class Solution {
 public:
     bool helper(TreeNode* root, long long left, long long right) {
