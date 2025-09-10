0438417f6ab1dcb133c135f8927947a74d81afe1
kongjun <kongjun18@outlook.com>
2025-09-09 22:36:23 -0700

    update minimum-size-subarray-sum-209 and spiral-matrix-ii-59

--- spiral-matrix-ii-59.cc
+++ spiral-matrix-ii-59.cc
@@ -34,3 +34,32 @@ public:
     return res;
   }
 };
+
+// 外圈循环完，在内圈重复循环。所有，先硬编码写出最外围的一圈，然后再修改 loop
+// 条件，往内圈走。
+//
+// class Solution {
+// public:
+//     vector<vector<int>> generateMatrix(int n) {
+//         vector<vector<int>> matrix(n, vector<int>(n));
+//         int c = 1;
+//         int k = n / 2 + (n %2 != 0);
+//         for (int j = 0; j < k; j++){
+//             for (int i = j; i < n -j; i++) {
+//              matrix[j][i] = c++;
+//             }
+//             for (int i = 1+j; i < n-j; i++) {
+//                 matrix[i][n-1-j] = c++;
+//             }
+//             for (int i=n-1 -1 -j; i >= (0+j); i--) {
+//                 matrix[n-1 -j][i] = c++;
+//
+//             }
+//             for (int i = n-1 -1 -j; i > (0+j); i--) {
+//                 matrix[i][0+j] = c++;
+//             }
+//         }
+//         return matrix;
+//
+//     }
+// };
