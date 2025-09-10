126141a7a370a540844ac2ec669735cf6a04de79
kongjun <kongjun18@outlook.com>
2025-09-10 15:31:57 -0700

    update reverse-string-ii-541

--- reverse-string-ii-541.cc
+++ reverse-string-ii-541.cc
@@ -1,3 +1,4 @@
+// 1. 迭代
 class Solution {
 public:
   string reverseStr(string s, int k) {
@@ -17,4 +18,26 @@ private:
       swap(s[start++], s[--end]);
     }
   }
+};
+
+// 2. 递归
+class Solution {
+    void reverseStr(string &s, size_t pos, int k) {
+        if (pos >= s.size()) {
+            return;
+        }
+        int left = pos, right = min(static_cast<size_t>(pos+k-1), s.size()-1);
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+        reverseStr(s, pos + 2*k, k);
+
+    }
+public:
+    string reverseStr(string s, int k) {
+        reverseStr(s, 0, k);
+        return s;
+    }
 };
