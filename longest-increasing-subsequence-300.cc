cf89dfc5b77a8613da8bad73f21e668ee6291705
kongjun <kongjun18@outlook.com>
2025-09-23 00:10:19 -0700

    update longest-increasing-subsequence-300

--- longest-increasing-subsequence-300.cc
+++ longest-increasing-subsequence-300.cc
@@ -1,20 +1,22 @@
+// Dynamic Programming
+//
+// Let dp[i] denotes the strictly increasing subsequence whose boundary is at i.
+// We will notice that dp[i] cannot be transitioned from dp[i-1], as strictly
+// increasing subsequence does not require contiguous. dp[i] may be transitioned
+// from dp[j] if nums[i] > nums[j] for all j < i. nums[j] is right boundary of
+// the subsequence, which is gauranteed by the definition of dp[i].
 class Solution {
 public:
   int lengthOfLIS(vector<int> &nums) {
-    if (nums.size() < 2) {
-      return nums.size();
-    }
     vector<int> dp(nums.size(), 1);
-    int result = 0;
+    int max_len = 0;
     for (int i = 0; i < nums.size(); ++i) {
       for (int j = 0; j < i; ++j) {
         if (nums[j] < nums[i]) {
           dp[i] = max(dp[i], dp[j] + 1);
         }
       }
-      if (result < dp[i]) {
-        result = dp[i];
-      }
+      max_len = max(max_len, dp[i]);
     }
     return result;
   }
