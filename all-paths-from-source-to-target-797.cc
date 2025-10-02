7c036fddc94d92028413baa3bfdeb6dfb10dc3ce
kongjun <kongjun18@outlook.com>
2025-10-02 10:50:39 -0700

    update all-paths-from-source-to-target-797

--- all-paths-from-source-to-target-797.cc
+++ all-paths-from-source-to-target-797.cc
@@ -1,4 +1,4 @@
-// DFS 模版题
+// DFS 模版题 推荐backtracking写法
 //
 // 类似于遍历树找一条合法路径，从根 DFS，记录路径遇到叶子节（这里是出度为0的节
 // 点）就将路径压入到路径数组中。
@@ -23,4 +23,30 @@ private:
     }
     path.pop_back();
   }
+};
+
+// Backtracking 写法
+class Solution {
+private:
+    vector<vector<int>> results;
+    vector<int> path;
+    void dfs(vector<vector<int>> &graph, int vertex) {
+        if (vertex == graph.size()-1) {
+            results.push_back(path);
+            return;
+        }
+
+        for (const auto adjacent : graph[vertex]) {
+            path.push_back(adjacent);
+            dfs(graph, adjacent);
+            path.pop_back();
+
+        }
+    }
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        path.push_back(0);
+        dfs(graph, 0);
+        return results;
+    }
 };
